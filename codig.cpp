ad9150847de437ba041a092cf3faa1d264455dff
john <teroristadelmal@hotmail.com>
2018-08-13 13:52:42 -0400

    segundo commit

--- codig.cpp
+++ codig.cpp
@@ -3,5 +3,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	
+	cout<<"cambio"<<endl;
+	
 	return 0;
 }
